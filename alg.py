080d3ea25373f70280efee56ebc232a84c82dcfc
web-penguin <a.sahabutdinov@mail.ru>
2018-04-13 22:07:33 +0300

    algorithm update

--- alg.py
+++ alg.py
@@ -6,6 +6,8 @@ tmp = ""
 for line in sys.stdin:
 	tmp += line
 
-f = open("/home/arias/diploma/datasets/" + str(hashlib.md5(tmp.encode("utf-8")).hexdigest()), 'w')
+filename = "/home/arias/diploma/datasets/" + str(hashlib.md5(tmp.encode("utf-8")).hexdigest())
+f = open(filename, 'w')
 f.write(tmp)
 f.close()
+print(filename, end='')
